741311eb127cffd6236c1d0bf1f85c2af7fdf88a
IgorGMoraes <igorgomesmoraes@gmail.com>
2019-09-01 13:12:27 -0300

    Login screen created

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import './Login.css'
 import logo from '../assets/logo.svg'
 
-export default function Login(){
-    return(
-        <img src={logo} alt="Tindev" />
+export default function Login() {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="Tindev" />
+            <form>
+                <input
+                    placeholder= "Digite seu usuário do GitHub"
+                />
+                <button type="submit">ENTRAR</button>
+            </form>
+        </div>
+
     )
 }
